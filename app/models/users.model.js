1f341777aa390acbe0746b20c485b61fa7b8a54a
Khaleedabuhawwas92 <42714078+Khaleedabuhawwas92@users.noreply.github.com>
2023-02-22 00:54:20 +0200

    21/2023

--- app/models/users.model.js
+++ app/models/users.model.js
@@ -1,32 +1,36 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 module.exports = (mongoose) => {
-   var schema = mongoose.Schema(
-      {
-         name: { type: String, required: true, trim: true },
-         email: { type: String, required: true, trim: true },
-         age: { type: Number, required: true },
-         password: { type: String, required: true },
-         auth: { type: String },
-         published: Boolean,
-         isAdmain: Boolean,
-      },
-      { timestamps: true }
-   );
+  var schema = mongoose.Schema(
+    {
+      name: { type: String, required: true, trim: true },
+      email: { type: String, required: true, trim: true },
+      password: { type: String, required: true },
+      isSuperAdmain: Boolean,
+      isAdmain: Boolean,
+      myCourses: [{ name: String }],
+      previousQuiz: [{ title: String }],
+      noteQuiz: [{ QuizTitle: String }],
+      resetQuiz: [{ type: Number}],
+      flashCard: [{ type: String }],
+   
+    },
+    { timestamps: true }
+  );
 
-   schema.method("toJSON", function () {
-      const { __v, _id, ...object } = this.toObject();
-      object.id = _id;
-      return object;
-   });
+  schema.method('toJSON', function () {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+  });
 
-   schema.method("token", function () {
-      const token = jwt.sign(
-         { _id: this._id, isAdmain: this.isAdmain },
-         "privet"
-      );
-      return token;
-   });
+//   schema.method('token', function () {
+//     const token = jwt.sign(
+//       { _id: this._id, isAdmain: this.isAdmain },
+//       'privet'
+//     );
+//     return token;
+//   });
 
-   const Users = mongoose.model("users", schema);
-   return Users;
+  const Users = mongoose.model('users', schema);
+  return Users;
 };
