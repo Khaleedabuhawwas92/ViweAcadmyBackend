b5dbcf0cb49ac83e78f16a494a5cbb206fedf63e
Khaleedabuhawwas92 <42714078+Khaleedabuhawwas92@users.noreply.github.com>
2023-02-22 12:24:19 +0200

    login iusse 22/2/2023

--- app/controllers/authorization.controller.js
+++ app/controllers/authorization.controller.js
@@ -1,48 +1,66 @@
-const db = require("../models");
-const serves = require("../services/serversUser");
+const db = require('../models');
+const serves = require('../services/serversUser');
 const User = db.user;
 
 exports.findUser = async (req, res) => {
    try {
-      let user = await User.findOne({ email: req.body.email });
-      const match = await serves.checkUser(req.body.password, user.password);
-      if (!user) {
-         res.status(404).send("Invalid email ");
+      // check if the user exists
+      const user = await User.findOne({ email: req.body.email  });
+      if (user) {
+        //check if password matches
+        const result = req.body.password === user.password;
+        if (result) {
+         res.redirect('/');
+        } else {
+          res.status(400).json({ error: "password doesn't match" });
+        }
+      } else {
+        res.status(400).json({ error: "User doesn't exist" });
       }
-      if (!match) {
-         res.status(404).send("invalid passwor");
-      }
-      const token = user.token();
-      const newToken = (user.auth = token);
-      user.save(newToken);
-      // res.header("Authorization", newToken);
-      res.send(token);
-   } catch (error) {
-      res.status(404).send("user not found");
-      console.log("user not found");
-   }
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+
+    
+//   try {
+//     let user = await User.findOne({ email: req.body.email });
+//     const match = req.body.password === user.password;
+//     if (!user) {
+//       res.status(404).send('Invalid email ');
+//     }
+//     if (!match) {
+//       res.status(404).send('invalid passwor');
+//     }
+//     // const token = user.token();
+//     // const newToken = (user.auth = token);
+//     // user.save(newToken);
+//     // res.header("Authorization", newToken);
+//     res.send('Login completed successfully');
+//   } catch (error) {
+//     res.status(404).send('user not found');
+//     console.log('user not found');
+//   }
 };
 
 exports.getUser = async (req, res) => {
-   const token = await req.header("Authorization");
-   try {
-      var selectedData = {
-         __v: false,
-         _id: false,
-         auth: false,
-      };
-      await User.findOne({ auth: token }, selectedData)
-         .then((data) => {
-            res.send(data);
-         })
-         .catch((err) => {
-            res.status(500).send({
-               message:
-                  err.message ||
-                  "Some error occurred while retrieving calenders.",
-            });
-         });
-   } catch (error) {
-      console.log(error);
-   }
+  const token = await req.header('Authorization');
+  try {
+    var selectedData = {
+      __v: false,
+      _id: false,
+      auth: false,
+    };
+    await User.findOne({ auth: token }, selectedData)
+      .then((data) => {
+        res.send(data);
+      })
+      .catch((err) => {
+        res.status(500).send({
+          message:
+            err.message || 'Some error occurred while retrieving calenders.',
+        });
+      });
+  } catch (error) {
+    console.log(error);
+  }
 };
